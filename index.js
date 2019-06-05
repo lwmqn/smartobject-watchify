eae09d939ea1c6e3dfc3b9d84faa9fd0e9c40b2a
Amer Alimanović <x@amer8.com>
2019-06-05 19:12:05 +0200

    Add standardjs and fix lint errors

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-var util = require('util'),
-    EventEmitter = require('events');
+const EventEmitter = require('events')
 
-module.exports = function watchify(so) {
-    Object.defineProperty(so, '_emitter', { writable: true, enumerable: false, configurable: false, value: new EventEmitter() });
-    Object.defineProperty(so, '_originalWrite', { writable: true, enumerable: false, configurable: true, value: so.write });
-    Object.defineProperty(so, '_emit', { writable: true, enumerable: false, configurable: false, value: so._emitter.emit });
+module.exports = function watchify (so) {
+  Object.defineProperty(so, '_emitter', {
+    writable: true, enumerable: false, configurable: false, value: new EventEmitter()
+  })
+  Object.defineProperty(so, '_originalWrite', {
+    writable: true, enumerable: false, configurable: true, value: so.write
+  })
+  Object.defineProperty(so, '_emit', {
+    writable: true, enumerable: false, configurable: false, value: so._emitter.emit
+  })
 
-    so.write = function (oid, iid, rid, value, opt, callback) {
-            var self = this;
+  so.write = function (oid, iid, rid, value, opt, callback) {
+    const self = this
 
-            if (typeof opt === 'function') {
-                callback = opt;
-                opt = undefined;
-            }
+    if (typeof opt === 'function') {
+      callback = opt
+      opt = undefined
+    }
 
-            this.read(oid, iid, rid, function (er, oldValue) {
-                self._originalWrite(oid, iid, rid, value, opt, function (err, currentValue) {
-                    if (!err && (currentValue !== undefined)) {
-                        if (currentValue !== oldValue) {
-                            var iPath = oid + '/' + iid + '/';
-                            self._emit(iPath, { iPath: iPath, rid: rid, cVal: currentValue, pVal: oldValue });
-                            self._emit(oid + '/' + iid + '/' + rid, currentValue, oldValue);
-                        }
-                    }
-                    callback(err, currentValue);
-                });
-            });
-    };
+    this.read(oid, iid, rid, (er, oldValue) => {
+      self._originalWrite(oid, iid, rid, value, opt, (err, currentValue) => {
+        if (!err && (currentValue !== undefined)) {
+          if (currentValue !== oldValue) {
+            const iPath = `${oid}/${iid}/`
+            self._emit(iPath, {
+              iPath, rid, cVal: currentValue, pVal: oldValue
+            })
+            self._emit(`${oid}/${iid}/${rid}`, currentValue, oldValue)
+          }
+        }
+        callback(err, currentValue)
+      })
+    })
+  }
 
-    so.onChange = so._emitter.on;
-    so.onChangeOnce = so._emitter.once;
+  so.onChange = so._emitter.on
+  so.onChangeOnce = so._emitter.once
 
-    so.removeListener = so._emitter.removeListener;
-    so.removeAllListeners = so._emitter.removeAllListeners;
+  so.removeListener = so._emitter.removeListener
+  so.removeAllListeners = so._emitter.removeAllListeners
 
-    return so;
-};
+  return so
+}
--- test/smartobject.test.js
+++ test/smartobject.test.js
@@ -1,140 +1,136 @@
-var chai = require('chai'),
-    expect = chai.expect,
-    EventEmitter = require('events'),
-    SmartObject = require('smartobject'),
-    watchify = require('../index.js');
+/* eslint-env mocha */
+const chai = require('chai')
+const { expect } = chai
+const SmartObject = require('smartobject')
+const watchify = require('../index.js')
 
-describe('Functional Check', function () {
-    var so = new SmartObject();
-    so = watchify(so);
+describe('Functional Check', () => {
+  let so = new SmartObject()
+  so = watchify(so)
 
-    it('should be inherited from SmartObject', function () {
-        expect(so instanceof SmartObject).to.be.equal(true);
-    });
+  it('should be inherited from SmartObject', () => {
+    expect(so instanceof SmartObject).to.be.equal(true)
+  })
 
-    it('should be composition of a EventEmitter', function () {
-        expect(so._emit).to.be.a('function');
-        expect(so.onChange).to.be.a('function');
-        expect(so.onChangeOnce).to.be.a('function');
-        expect(so.removeListener).to.be.a('function');
-        expect(so.removeAllListeners).to.be.a('function');
-    });
+  it('should be composition of a EventEmitter', () => {
+    expect(so._emit).to.be.a('function')
+    expect(so.onChange).to.be.a('function')
+    expect(so.onChangeOnce).to.be.a('function')
+    expect(so.removeListener).to.be.a('function')
+    expect(so.removeAllListeners).to.be.a('function')
+  })
 
-    it('should fire and listened via onChange correctly', function (done) {
-        so.onChange('hello', function (data) {
-            if (data === 1)
-                done();
-        });
+  it('should fire and listened via onChange correctly', (done) => {
+    so.onChange('hello', (data) => {
+      if (data === 1) done()
+    })
 
-        setImmediate(function () {
-            so._emit('hello', 1);
-        });
-    });
+    setImmediate(() => {
+      so._emit('hello', 1)
+    })
+  })
 
-    it('should fire and listened via on correctly', function (done) {
-        so.onChange('helloY', function (data) {
-            if (data === 1)
-                done();
-        });
+  it('should fire and listened via on correctly', (done) => {
+    so.onChange('helloY', (data) => {
+      if (data === 1) done()
+    })
 
-        setImmediate(function () {
-            so._emit('helloY', 1);
-        });
-    });
+    setImmediate(() => {
+      so._emit('helloY', 1)
+    })
+  })
 
-    it('should init correctly', function () {
-        so.init('temperature', 0, {
-            sensorValue: 31,
-            units : 'C'
-        });
+  it('should init correctly', () => {
+    so.init('temperature', 0, {
+      sensorValue: 31,
+      units: 'C'
+    })
 
-        so.init('temperature', 1, {
-            _state: {           // inner state
-                foo: 'bar'
-            },
-            sensorValue: 75,
-            units : 'F'
-        });
+    so.init('temperature', 1, {
+      _state: { // inner state
+        foo: 'bar'
+      },
+      sensorValue: 75,
+      units: 'F'
+    })
 
-        so.init(3303, 18, {
-            sensorValue: 301,
-            units : 'K'
-        }, function () {
-            // this._state is an empty object by default
-            // you can attach things to it
-            this._state.foo = 'bar';
-        });
+    so.init(3303, 18, {
+      sensorValue: 301,
+      units: 'K'
+    }, function () {
+      // this._state is an empty object by default
+      // you can attach things to it
+      this._state.foo = 'bar'
+    })
 
-        expect(so.get('temperature', 0, 'sensorValue')).to.be.equal(31);
-        expect(so.get('temperature', 1, 'sensorValue')).to.be.equal(75);
-        expect(so.get(3303, 18, 'sensorValue')).to.be.equal(301);
-    });
+    expect(so.get('temperature', 0, 'sensorValue')).to.be.equal(31)
+    expect(so.get('temperature', 1, 'sensorValue')).to.be.equal(75)
+    expect(so.get(3303, 18, 'sensorValue')).to.be.equal(301)
+  })
 
-    it('should report change of temperature/1/sensorValue correctly', function (done) {
-        so.onChange('temperature/1/sensorValue', function (cVal, pVal) {
-            if (cVal === 80 && pVal === 75)
-                done();
-        });
-        so.write('temperature', 1, 'sensorValue', 80, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of temperature/1/sensorValue correctly', (done) => {
+    so.onChange('temperature/1/sensorValue', (cVal, pVal) => {
+      if (cVal === 80 && pVal === 75) done()
+    })
+    so.write('temperature', 1, 'sensorValue', 80, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-    it('should report change of temperature/1/ correctly', function (done) {
-        so.onChange('temperature/1/', function (rRec) {
-            if (rRec.rid === 'sensorValue' && rRec.cVal === 30 && rRec.pVal === 80)
-                done();
-        });
-        so.write('temperature', 1, 'sensorValue', 30, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of temperature/1/ correctly', (done) => {
+    so.onChange('temperature/1/', (rRec) => {
+      if (rRec.rid === 'sensorValue' && rRec.cVal === 30 && rRec.pVal === 80) done()
+    })
+    so.write('temperature', 1, 'sensorValue', 30, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-    it('should report change of temperature/0/sensorValue correctly', function (done) {
-        so.onChange('temperature/0/sensorValue', function (cVal, pVal) {
+  it('should report change of temperature/0/sensorValue correctly', (done) => {
+    so.onChange('temperature/0/sensorValue', (cVal, pVal) => {
+      if (cVal === 12 && pVal === 31) done()
+    })
+    so.write('temperature', 0, 'sensorValue', 12, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-            if (cVal === 12 && pVal === 31)
-                done();
-        });
-        so.write('temperature', 0, 'sensorValue', 12, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of temperature/0/sensorValue correctly', (done) => {
+    so.onChange('temperature/0/sensorValue', (cVal, pVal) => {
+      if (cVal === 55 && pVal === 12) done()
+    })
+    so.write('temperature', 0, 'sensorValue', 55, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-    it('should report change of temperature/0/sensorValue correctly', function (done) {
-        so.onChange('temperature/0/sensorValue', function (cVal, pVal) {
-            if (cVal === 55 && pVal === 12)
-                done();
-        });
-        so.write('temperature', 0, 'sensorValue', 55, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
+  it('should report change of 3303/18/sensorValue correctly', (done) => {
+    so.onChange('3303/18/sensorValue', (cVal, pVal) => {
+      if (cVal === 307 && pVal === 301) done()
+    })
+    so.write('3303', 18, 'sensorValue', 307, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
 
-    it('should report change of 3303/18/sensorValue correctly', function (done) {
-        so.onChange('3303/18/sensorValue', function (cVal, pVal) {
-            if (cVal === 307 && pVal === 301)
-                done();
-        });
-        so.write('3303', 18, 'sensorValue', 307, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
-
-
-    it('should report change of 3303/18/ correctly', function (done) {
-        so.onChange('3303/18/', function (rRec) {
-            if (rRec.rid === 'sensorValue' && rRec.cVal === 99 && rRec.pVal === 307)
-                done();
-        });
-        so.write('3303', 18, 'sensorValue', 99, function (err, data) {
-            // console.log(err);
-            // console.log(data);
-        });
-    });
-});
+  it('should report change of 3303/18/ correctly', (done) => {
+    so.onChange('3303/18/', (rRec) => {
+      if (rRec.rid === 'sensorValue' && rRec.cVal === 99 && rRec.pVal === 307) done()
+    })
+    so.write('3303', 18, 'sensorValue', 99, (err, data) => {
+      expect(err).to.be.equal(null)
+      // console.log(err);
+      // console.log(data);
+    })
+  })
+})
